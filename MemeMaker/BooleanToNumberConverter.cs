d474ae9b057609ba3479778d29d734f2d7fff09e
MarceloCor98 <ma.cordero@iesdoctorbalmis.com>
2019-10-23 10:44:26 +0200

    Agregar archivos de proyecto.

---
+++ MemeMaker/BooleanToNumberConverter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace MemeMaker
+{
+    public class BooleanToNumberConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool)
+            {
+                if ((bool)value)
+                    return 1;
+                else
+                    return 0;
+            }
+
+            return 0;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if ((int)value == 1)
+                return true;
+            else
+                return false;
+        }
+    }
+}
